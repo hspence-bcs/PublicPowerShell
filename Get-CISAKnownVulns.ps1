dfec48ac84b4a5fd0b7560f123b7058ad7b9ca5c
Dave Spence <hspence@bcswan.net>
2025-03-14 09:09:38 -0400

    Added comments and header.

--- Get-CISAKnownVulns.ps1
+++ Get-CISAKnownVulns.ps1
@@ -1,6 +1,47 @@
-﻿function Get-CISAKnownVulnerabilities {
+﻿<#
+.SYNOPSIS
+    Retrieves known exploited vulnerabilities from the CISA CSV file.
+
+.DESCRIPTION
+    This script fetches the known exploited vulnerabilities from the specified CISA CSV file URL.
+    It allows filtering the data by application, vendor, and CVE ID.
+
+.PARAMETER CISAcsv
+    The URL of the CSV file containing known exploited vulnerabilities. Default is the CISA URL.
+
+.PARAMETER Application
+    The name of the application to filter the vulnerabilities by.
+
+.PARAMETER Vendor
+    The name of the vendor to filter the vulnerabilities by.
+
+.PARAMETER CVE
+    The CVE ID to filter the vulnerabilities by.
+
+.EXAMPLE
+    Get-CISAKnownVulnerabilities -Application "ExampleApp"
+    Retrieves vulnerabilities for the specified application.
+
+.EXAMPLE
+    Get-CISAKnownVulnerabilities -Vendor "ExampleVendor"
+    Retrieves vulnerabilities for the specified vendor.
+
+.EXAMPLE
+    Get-CISAKnownVulnerabilities -CVE "CVE-2021-12345"
+    Retrieves vulnerabilities for the specified CVE ID.
+
+.EXAMPLE
+    Get-CISAKnownVulnerabilities -Application "ExampleApp" -Vendor "ExampleVendor" -CVE "CVE-2021-12345"
+    Retrieves vulnerabilities for the specified application, vendor, and CVE ID.
+
+.NOTES
+    Author: Dave Spence
+    Date: August 21, 2024
+#>
+
+function Get-CISAKnownVulnerabilities {
     param (
-        [string]$CISAcsv = 'https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv',  # URL of the CSV file
+        [string]$CISAcsv = 'https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv',
         [string]$Application,
         [string]$Vendor,
         [string]$CVE
@@ -37,13 +78,10 @@
 }
 
 # Example usage with application filter
-Get-CISAKnownVulnerabilities -Application "ExampleApp"
+Get-CISAKnownVulnerabilities -Application "IPV6"
 
 # Example usage with vendor filter
-Get-CISAKnownVulnerabilities -Vendor "ExampleVendor"
+Get-CISAKnownVulnerabilities -Vendor "Dell"
 
 # Example usage with CVE filter
-Get-CISAKnownVulnerabilities -CVE "CVE-2021-12345"
-
-# Example usage with all filters
-Get-CISAKnownVulnerabilities -Application "ExampleApp" -Vendor "ExampleVendor" -CVE "CVE-2021-12345"
+Get-CISAKnownVulnerabilities -CVE "CVE-2018-0125"
